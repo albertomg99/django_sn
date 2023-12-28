8f8ac6eca5a061ccc01e7a5b1d93f5563b5c53dc
Alberto Martinez <albertomg@gmail.com>
2023-12-28 22:01:24 +0100

    Inicial

---
+++ AMGTeer/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+    inlines = [ProfileInLine]
+
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
